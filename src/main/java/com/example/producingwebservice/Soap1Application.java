562cd3155f734f3ef94a5995f3f27ad4a0ba1cfb
Marah Shabib <MarahSh@exalt.ps>
2021-02-02 10:27:09 +0200

    first commit

---
+++ src/main/java/com/example/producingwebservice/Soap1Application.java
@@ -0,0 +1,13 @@
+package com.example.producingwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Soap1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Soap1Application.class, args);
+    }
+
+}
